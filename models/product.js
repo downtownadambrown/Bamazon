4726a581063ce69f6a5fd321c240aa14b2da4a9c
Adam Brown <downtownadambrown@gmail.com>
2018-11-02 16:35:08 -0400

    Built DB schema model for Products via Sequelize

---
+++ models/product.js
@@ -0,0 +1,34 @@
+module.exports = function (connection, Sequelize) {
+    var Product = connection.define('Product', {
+        product_name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        department_name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        price: {
+            type: Sequelize.FLOAT(10,2),
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        stock_quantity: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        }
+    });
+    
+    return Product;
+};
