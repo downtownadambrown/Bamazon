1b17e02ec41a50f64af81a5bca103fe6567b0123
Adam Brown <downtownadambrown@gmail.com>
2018-11-08 23:20:25 -0500

    Completed PUT routes and UI usage

--- public/js/app.js
+++ public/js/app.js
@@ -27,7 +27,6 @@ const clearComponents = function () {
 
 const renderProduct = function () {
     const thisRef = this;
-    console.log(this);
     
     $.ajax({
         url: '/api/product',
@@ -48,7 +47,7 @@ const renderProduct = function () {
                                     <th scope="col">Product Name</th>
                                     <th scope="col">Dept Name</th>
                                     <th scope="col">Quantity</th>
-                                    <th scope="col">Price</th>
+                                    <th scope="col">Unit Price</th>
                                 </tr>`);
             $('#admin-table-header').append(newHeader);
 
@@ -66,24 +65,101 @@ const renderProduct = function () {
             let newHeader = $(`<tr id="admin-table-header-row">
                                     <th scope="col">Product Name</th>
                                     <th scope="col">Current Qty</th>
-                                    <th scope="col">Price/unit</th>
+                                    <th scope="col">Unit Price</th>
                                     <th scope="col" class="text-center">Add to your order</th>
+                                    <th scope="col">Subtotal</th>
                                 </tr>`);
             $('#admin-table-header').append(newHeader);
-            rows.forEach(function (row) {
+            console.log(rows);
+            for (let i = 0; i < rows.length; i++){
                 let newRow = $(`<tr class="admin-table-row">
-                                    <td>${row.product_name}</td>
-                                    <td>${row.stock_quantity}</td>
-                                    <td>$${row.price}</td>
-                                    <td class="p-0 text-center align-middle"><i class="fas fa-minus-circle p-1"></i>0<i class="fas fa-plus-circle p-1"></i></td>
+                                    <td id="${rows[i].id}" class="rowID">${rows[i].product_name}</td>
+                                    <td id="quantity${i}">${rows[i].stock_quantity}</td>
+                                    <td id="price${i}">$${rows[i].price}</td>
+                                    <td class="text-center d-flex justify-content-center align-middle">
+                                        <div id="miniwrapper" class="align-middle d-flex flex-row align-items-center justify-content-between">
+                                            <div class="minButton"><i id="minus${i}" class="fas fa-minus-circle p-1"></i></div>
+                                            <div class="addQty" id="addQty${i}">0</div>
+                                            <div class="addButton"><i id="plus${i}" class="fas fa-plus-circle p-1"></i></div>
+                                        </div>
+                                    </td>
+                                    <td id="subtotal${i}">$0.00</td>
                                 </tr>`);
                 $('#admin-table-body').append(newRow);
-            });
+                $(`#minus${i}`).on('click', function(){ 
+
+                    // Making sure they aren't trying to go below zero first
+                    // If they are -- warn them w/ red blinky blinky text
+                    let addQuantity = parseInt($(`#addQty${i}`).text());
+                    if (addQuantity === 0){
+                        $(`#addQty${i}`).css('color', 'red').fadeOut('normal', function() {
+                            $(this).fadeIn('normal', function() {
+                                $(this).css('color', 'black');
+                            });
+                        });
+                        return;
+                    }
+
+                    // Grabbing the Quantity for modification
+                    let qty = parseInt($(`#addQty${i}`).text());
+                    qty--;                
+                    $(`#addQty${i}`).text(qty);
+
+                    // Grabbing HTML data to modify SubTotal per unit price
+                    let price = $(`#price${i}`).text().slice(1);
+                    const currentSubtotal = $(`#subtotal${i}`).text().slice(1);
+                    const newprice = Number(price);                 
+                    let newSubtotal = parseFloat(currentSubtotal) - newprice;
+                    newSubtotal = newSubtotal.toFixed(2);
+
+                    $(`#subtotal${i}`).text(`$${newSubtotal}`);
+                });
+                $(`#plus${i}`).on('click', function(){
+                    // Grabbing the Quantity for modification
+                    let qty = parseInt($(`#addQty${i}`).text());
+                    qty++;                
+                    $(`#addQty${i}`).text(qty);
+                    
+                    // Grabbing HTML data to modify SubTotal per unit price
+                    let price = $(`#price${i}`).text().slice(1);
+                    const currentSubtotal = $(`#subtotal${i}`).text().slice(1);
+                    const newprice = Number(price);                 
+                    let newSubtotal = parseFloat(currentSubtotal) + newprice;
+                    newSubtotal = newSubtotal.toFixed(2);
+
+                    $(`#subtotal${i}`).text(`$${newSubtotal}`);
+                });
+            }
             
-            $('#submitButton').fadeIn();
+            $('#submitButton').fadeIn().on('click', submitOrder);
         } else {
             throw new Error();
         }
+    });
+};
+
+const submitOrder = function(){
+
+    let putData = { data : []};
+    let activeRows = document.querySelectorAll('.rowID');
+    
+    for (let i = 0; i < activeRows.length; i++){
+        const idKey = activeRows[i].id;
+        const qtyToAdd = parseInt($(`#addQty${idKey-1}`).text());
+        const currentQty = parseInt($(`#quantity${idKey-1}`).text());
+
+        if (qtyToAdd > 0){
+            putData.data.push({
+                id : idKey,
+                qty : qtyToAdd + currentQty
+            });
+        }
+    }
+
+    $.ajax({
+        url: '/api/product',
+        type: 'PUT',
+        data: putData
     });
 };
 
@@ -93,7 +169,7 @@ const addProduct = function () {
         "department_name": $('#prod-department-name').val(),
         "price": $('#prod-price').val(),
         "stock_quantity": $('#prod-stock-quantity').val()
-    }
+    };
 
     $.ajax({
         url: '/api/product',
@@ -115,9 +191,6 @@ const addProduct = function () {
 };
 
 const addInventory = function () {
-
-    //add submit button
-
 
 /*     $.ajax({
         url: '/api/product',
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -11,14 +11,11 @@ module.exports = function(app) {
         db.Product.findAll({}).then(function(rows){
             res.json(rows);
         }).catch(function(err){
-            res.json({ error: error });
+            res.json({ error: err });
         });
     });
 
     app.post('/api/product', function(req, res){
-
-        console.log('request: ', req.body);
-
         db.Product.create(req.body)
         .then(function(rows){
             res.json({ success: true,
@@ -26,5 +23,24 @@ module.exports = function(app) {
         }).catch(function(err){
             res.json({ error: err });
         });
+    });
+
+    app.put('/api/product', function(req, res){
+        console.log(req.body);
+        for (let i = 0; i < req.body.data.length; i++){
+
+            let sq = parseInt(req.body.data[i].qty);
+            console.log('stock qty', sq, typeof sq)
+
+            let theID = req.body.data[i].id;
+            console.log('id', theID, typeof theID);
+
+            db.Product.update({ stock_quantity : sq
+            },{ where : { id : theID }
+            }).catch(function(err){
+                throw new Error();
+            });
+        }
+        res.json('done');
     });
 };
